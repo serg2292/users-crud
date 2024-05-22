ef29400a86e46e12156dd4f1e9cda42c22fdbf4f
Sergey Sidorov <sergit2292@gmail.com>
2024-05-22 14:36:46 +0300

    some refactor

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
 const http = require('http');
-const { createUsersTable } = require('./users/database/usersDB');
 const usersRoutes = require('./users/routes/usersRoutes');
 
-const PORT = 3000;
-const ADDRESS = '127.0.0.1';
+function server() {
+    const PORT = 3000;
+    const ADDRESS = '127.0.0.1';
+    const server = http.createServer(async (req, res) => {
+        const url = req.url
+        if (url.startsWith('/users')) {
+            usersRoutes(req, res)
+        }
+    });
 
-createUsersTable();
+    server.listen(PORT, ADDRESS, () => {
+        console.log(`Listening on ${ADDRESS}:${PORT}`);
+    });
+}
 
-const server = http.createServer(async (req, res) => {
-    const url = req.url
-    if (url.startsWith('/users')) {
-        usersRoutes(req, res)
-    }
-});
-
-server.listen(PORT, ADDRESS, () => {
-    console.log(`Listening on ${ADDRESS}:${PORT}`);
-});
+module.exports = server
